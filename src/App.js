47bf6665d87c511af89448a44f98d18fb72f8ccb
DashinIN <green19grant@yandex.ru>
2023-06-08 16:11:15 +0300

    initial counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-    hi
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>0</h1>
+        <button className="minus">- Минус</button>
+        <button className="plus">Плюс +</button>
+      </div>
     </div>
   );
 }
