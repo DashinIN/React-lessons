7cf0c89df607c864e8a022d11bfc76e0f0908e77
DashinIN <green19grant@yandex.ru>
2023-06-15 12:52:58 +0300

    counter and field done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import './App.scss';
 function App() {
 
   const [count, setCount] = useState(0);
-  
+  const [text, setText] = useState('Привет!');
+
   return (
     <div className="App">
-      <div>
+      <div className="wrapper">
         <h2>Счетчик:</h2>
         <h1>{count}</h1>
         <button className="minus" onClick={() => setCount(prevCount => prevCount - 1 )}>- Минус</button>
         <button className="plus" onClick={() => setCount(prevCount => prevCount + 1 )} >Плюс +</button>
+      </div>
+
+      <div className="wrapper">
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <p>Вы ввели: {text}</p>
+        <button onClick={() => setText('Привет!')}>Вернуть как было</button>
       </div>
     </div>
   );
